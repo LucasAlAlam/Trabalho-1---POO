2de4c0a6ba14267ed3797284b6d783769bb378c9
o2du <eduardofalves2709@gmail.com>
2024-09-09 12:03:09 -0300

    Update Filme.java

--- Filme.java
+++ Filme.java
@@ -1,5 +1,8 @@
 import java.util.ArrayList;
-
+/**
+* atribuição de valores da classe filme
+* @author o2du
+*/
 public class Filme {
 
 	String titulo; // nome
@@ -12,7 +15,10 @@ public class Filme {
 	double avaliação;
 	int votos;
 	double bilheteria;
-
+/**
+* constructor
+* @author o2du
+*/
 	public Filme(String titulo, ArrayList<String> genero, String descrição, String diretor, ArrayList<String> atores, int ano, int duracao, double avaliação, int votos, double bilheteria) {
 		this.titulo = titulo;
 		this.genero = genero;
